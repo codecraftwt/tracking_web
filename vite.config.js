fafa60c1a2b8d0888c2dd744be21d02cea8a4d18
codecraft <codecraftwt@gmail.com>
2025-03-12 19:11:47 +0530

    vite preview new

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Ensure it's accessible from outside localhost
+    port: 4173,       // You can specify the port you want Vite to use
+  }
 })
