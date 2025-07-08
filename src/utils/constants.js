4d204528776607a371741ff1ec676cdb3075397a
codecraftwt <codecraftwt@gmail.com>
2025-07-08 19:04:18 +0530

    updated base url with live url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,9 +1,9 @@
 // export const baseURL = 'https://tracking.walstartechnologies.com/';
 export const API_KEY = "UEnJxClxfXyy2HYh_p9AdtvUUYDR0";
 
-export const baseURL = "http://localhost:5000/";
+// export const baseURL = "http://localhost:5000/";
 
-// export const baseURL = "https://trackingapp-backend-1.onrender.com/";
+export const baseURL = "https://trackingapp-backend-1.onrender.com/";
 
 // export const baseURL = "https://trackingapp.instantwebsitedevelopment.com/";
 
