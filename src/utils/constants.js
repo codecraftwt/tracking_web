d0e9cd62d133ddac9b1bc7dd54e581e0a95105c5
codecraftwt <codecraftwt@gmail.com>
2025-07-28 10:37:39 +0530

    updated base url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,9 +3,9 @@ export const API_KEY = "UEnJxClxfXyy2HYh_p9AdtvUUYDR0";
 
 // export const baseURL = "http://localhost:5000/";
 
-export const baseURL = "https://trackingapp-backend-1.onrender.com/";
+// export const baseURL = "https://trackingapp-backend-1.onrender.com/";
 
-// export const baseURL = "https://trackingapp.instantwebsitedevelopment.com/";
+export const baseURL = "https://trackingapp.instantwebsitedevelopment.com/";
 
 // Razorpay Configuration
 export const RAZORPAY_KEY_ID = "rzp_test_eXyUgxz2VtmepU";
